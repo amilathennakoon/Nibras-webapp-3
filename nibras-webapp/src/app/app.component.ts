422ca54819061490ac72be836393a0ad06de9a26
unknown <145asd@gmail.com>
2022-06-11 00:47:51 +0530

    improve responsiveness for web and mobile

--- nibras-webapp/src/app/app.component.ts
+++ nibras-webapp/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nibras-webapp';
-  selectedMenuItem: string;
+  selectedMenuItem: string='home';
 
   menuItemClicked(menuItem: string): void {
     this.selectedMenuItem = menuItem;
